821b7fbc5dbe27c56c9d8ef28dfafc4e4593b4b7
Progwml6 <progwml6@gmail.com>
2016-07-16 14:02:31 -0400

    move curse connections to a separate thread

--- src/main/java/engineer/carrot/warren/thump/cursevoice/CurseIntegration.java
+++ src/main/java/engineer/carrot/warren/thump/cursevoice/CurseIntegration.java
@@ -1,49 +1,24 @@
 package engineer.carrot.warren.thump.cursevoice;
 
-import com.feed_the_beast.javacurselib.common.enums.DevicePlatform;
-import com.feed_the_beast.javacurselib.data.Apis;
-import com.feed_the_beast.javacurselib.rest.RestUserEndpoints;
-import com.feed_the_beast.javacurselib.service.contacts.contacts.ChannelContract;
-import com.feed_the_beast.javacurselib.service.contacts.contacts.ContactsResponse;
-import com.feed_the_beast.javacurselib.service.contacts.contacts.GroupNotification;
-import com.feed_the_beast.javacurselib.service.logins.login.LoginRequest;
-import com.feed_the_beast.javacurselib.service.logins.login.LoginResponse;
-import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionRequest;
-import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionResponse;
-import com.feed_the_beast.javacurselib.utils.CurseGUID;
-import com.feed_the_beast.javacurselib.websocket.WebSocket;
-import com.feed_the_beast.javacurselib.websocket.messages.notifications.NotificationsServiceContractType;
 import engineer.carrot.warren.thump.api.IThumpMinecraftSink;
 import engineer.carrot.warren.thump.helper.LogHelper;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by progwml6 on 7/4/16.
  */
 public class CurseIntegration {
-    private String username;
-    private String password;
-    private RestUserEndpoints endpoints;
-    private ContactsResponse contacts;
-    private CreateSessionResponse session;
-    private WebSocket webSocket;
-    private String serverName;
-    private String channelName;
-    private CurseGUID channel;
-    private IThumpMinecraftSink sink;
+    protected String username;
+    protected String password;
+    protected String serverName;
+    protected String channelName;
+    protected IThumpMinecraftSink sink;
+    private CurseThread thread;
 
     public CurseIntegration (String username, String password, String serverName, String channelName, IThumpMinecraftSink sink) {
         this.username = username;
         this.password = password;
         this.serverName = serverName;
         this.channelName = channelName;
-        endpoints = new RestUserEndpoints();
-        endpoints.setupEndpoints();
         this.sink = sink;
 
     }
@@ -69,38 +44,24 @@ public class CurseIntegration {
     }
 
     public void init () {
-        try {
-            CountDownLatch latch = new CountDownLatch(1);
-            LoginResponse loginResponse = endpoints.doLogin(new LoginRequest(username, password));
-            endpoints.setAuthToken(loginResponse.session.token);
-            endpoints.setupEndpoints();
-            contacts = endpoints.contacts.get().get();
-            session = endpoints.session.create(new CreateSessionRequest(CurseGUID.newRandomUUID(), DevicePlatform.UNKNOWN)).get();
-            Optional<CurseGUID> id = contacts.getChannelIdbyNames(serverName, channelName, true);
-            if (id.isPresent()) {
-                channel = id.get();
-                webSocket = new WebSocket(loginResponse, session, new URI(Apis.NOTIFICATIONS));
-                webSocket.addTask(new CurseMessage(sink), NotificationsServiceContractType.CONVERSATION_MESSAGE_NOTIFICATION);
-                webSocket.start();
-            } else {
-                LogHelper.error("could not find channel ID for {}/{}. Not starting CV plugin", serverName, channelName);
-                latch.await();
-                return;
-            }
-            latch.await();
-        } catch (InterruptedException | ExecutionException | URISyntaxException e) {
-            LogHelper.error("error setting up curse plugin", e);
-        }
+        LogHelper.info("starting up curse integration");
+        thread = new CurseThread(this);
+        thread.setName("Thump - Curse");
+        thread.start();
         LogHelper.info("CV plugin has started up");
+    }
+
+    public void sendToCurse (String message) {
+        if (thread != null && thread.webSocket != null && thread.channel != null) {
+            thread.webSocket.sendMessage(thread.channel, message);
+        } else {
+            LogHelper.error("curse thread not configured. Could not send message {} to curse", message);
+        }
     }
 
     public void shutdown () {
         //TODO logout??
-        endpoints = new RestUserEndpoints();
-        endpoints.setupEndpoints();
+        thread.interrupt();
     }
 
-    public void sendToCurse (String message) {
-        webSocket.sendMessage(channel, message);
-    }
 }
---
+++ src/main/java/engineer/carrot/warren/thump/cursevoice/CurseThread.java
@@ -0,0 +1,70 @@
+package engineer.carrot.warren.thump.cursevoice;
+
+import com.feed_the_beast.javacurselib.common.enums.DevicePlatform;
+import com.feed_the_beast.javacurselib.data.Apis;
+import com.feed_the_beast.javacurselib.rest.RestUserEndpoints;
+import com.feed_the_beast.javacurselib.service.contacts.contacts.ContactsResponse;
+import com.feed_the_beast.javacurselib.service.logins.login.LoginRequest;
+import com.feed_the_beast.javacurselib.service.logins.login.LoginResponse;
+import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionRequest;
+import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionResponse;
+import com.feed_the_beast.javacurselib.utils.CurseGUID;
+import com.feed_the_beast.javacurselib.websocket.WebSocket;
+import com.feed_the_beast.javacurselib.websocket.messages.notifications.NotificationsServiceContractType;
+import engineer.carrot.warren.thump.helper.LogHelper;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by progwml6 on 7/16/16.
+ */
+public class CurseThread extends Thread {
+    private RestUserEndpoints endpoints;
+    private ContactsResponse contacts;
+    private CreateSessionResponse session;
+    protected WebSocket webSocket;
+    protected CurseGUID channel;
+    private CurseIntegration integration;
+
+    public CurseThread (CurseIntegration integration) {
+        this.integration = integration;
+    }
+
+    @Override
+    public void run () {
+        try {
+            endpoints = new RestUserEndpoints();
+            endpoints.setupEndpoints();
+
+            CountDownLatch latch = new CountDownLatch(1);
+            LoginResponse loginResponse = endpoints.doLogin(new LoginRequest(integration.username, integration.password));
+            endpoints.setAuthToken(loginResponse.session.token);
+            endpoints.setupEndpoints();
+            contacts = endpoints.contacts.get().get();
+            session = endpoints.session.create(new CreateSessionRequest(CurseGUID.newRandomUUID(), DevicePlatform.UNKNOWN)).get();
+            Optional<CurseGUID> id = contacts.getChannelIdbyNames(integration.serverName, integration.channelName, true);
+            if (id.isPresent()) {
+                channel = id.get();
+                webSocket = new WebSocket(loginResponse, session, new URI(Apis.NOTIFICATIONS));
+                webSocket.addTask(new CurseMessage(integration.sink), NotificationsServiceContractType.CONVERSATION_MESSAGE_NOTIFICATION);
+                LogHelper.info("starting curse websocket connection");
+                webSocket.start();
+                LogHelper.info("curse websocket connection started");
+
+            } else {
+                LogHelper.error("could not find channel ID for {}/{}. Not starting CV plugin", integration.serverName, integration.channelName);
+                latch.await();
+                return;
+            }
+            latch.await();
+        } catch (InterruptedException | ExecutionException | URISyntaxException e) {
+            LogHelper.error("curse plugin thread shutting down", e);
+        }
+    }
+
+
+}
