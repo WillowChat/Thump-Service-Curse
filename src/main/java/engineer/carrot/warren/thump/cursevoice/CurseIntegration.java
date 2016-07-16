93aedd55238f04b677b3494b23695b2db6fd553b
Progwml6 <progwml6@gmail.com>
2016-07-16 13:22:28 -0400

    fix several bugs that were causing crashes on mc server startup ignore server/channel name case when getting channel ID's

--- src/main/java/engineer/carrot/warren/thump/cursevoice/CurseIntegration.java
+++ src/main/java/engineer/carrot/warren/thump/cursevoice/CurseIntegration.java
@@ -3,7 +3,9 @@ package engineer.carrot.warren.thump.cursevoice;
 import com.feed_the_beast.javacurselib.common.enums.DevicePlatform;
 import com.feed_the_beast.javacurselib.data.Apis;
 import com.feed_the_beast.javacurselib.rest.RestUserEndpoints;
+import com.feed_the_beast.javacurselib.service.contacts.contacts.ChannelContract;
 import com.feed_the_beast.javacurselib.service.contacts.contacts.ContactsResponse;
+import com.feed_the_beast.javacurselib.service.contacts.contacts.GroupNotification;
 import com.feed_the_beast.javacurselib.service.logins.login.LoginRequest;
 import com.feed_the_beast.javacurselib.service.logins.login.LoginResponse;
 import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionRequest;
@@ -12,9 +14,11 @@ import com.feed_the_beast.javacurselib.utils.CurseGUID;
 import com.feed_the_beast.javacurselib.websocket.WebSocket;
 import com.feed_the_beast.javacurselib.websocket.messages.notifications.NotificationsServiceContractType;
 import engineer.carrot.warren.thump.api.IThumpMinecraftSink;
+import engineer.carrot.warren.thump.helper.LogHelper;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 
@@ -39,6 +43,7 @@ public class CurseIntegration {
         this.serverName = serverName;
         this.channelName = channelName;
         endpoints = new RestUserEndpoints();
+        endpoints.setupEndpoints();
         this.sink = sink;
 
     }
@@ -71,19 +76,28 @@ public class CurseIntegration {
             endpoints.setupEndpoints();
             contacts = endpoints.contacts.get().get();
             session = endpoints.session.create(new CreateSessionRequest(CurseGUID.newRandomUUID(), DevicePlatform.UNKNOWN)).get();
-            channel = contacts.getChannelIdbyNames(serverName, channelName).get();
-            webSocket = new WebSocket(loginResponse, session, new URI(Apis.NOTIFICATIONS));
-            webSocket.addTask(new CurseMessage(sink), NotificationsServiceContractType.CONVERSATION_MESSAGE_NOTIFICATION);
-            webSocket.start();
+            Optional<CurseGUID> id = contacts.getChannelIdbyNames(serverName, channelName, true);
+            if (id.isPresent()) {
+                channel = id.get();
+                webSocket = new WebSocket(loginResponse, session, new URI(Apis.NOTIFICATIONS));
+                webSocket.addTask(new CurseMessage(sink), NotificationsServiceContractType.CONVERSATION_MESSAGE_NOTIFICATION);
+                webSocket.start();
+            } else {
+                LogHelper.error("could not find channel ID for {}/{}. Not starting CV plugin", serverName, channelName);
+                latch.await();
+                return;
+            }
             latch.await();
         } catch (InterruptedException | ExecutionException | URISyntaxException e) {
-            //TODO error handling
+            LogHelper.error("error setting up curse plugin", e);
         }
+        LogHelper.info("CV plugin has started up");
     }
 
     public void shutdown () {
         //TODO logout??
         endpoints = new RestUserEndpoints();
+        endpoints.setupEndpoints();
     }
 
     public void sendToCurse (String message) {
