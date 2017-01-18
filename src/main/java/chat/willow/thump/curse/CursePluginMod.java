47dd6d606dc522848d9cdf5c37c9ed45490e54b1
Sky Welch <git@bunnies.io>
2017-01-18 18:39:59 +0000

    Require Thump > 4.5.0.5

---
+++ src/main/java/chat/willow/thump/curse/CursePluginMod.java
@@ -0,0 +1,7 @@
+package chat.willow.thump.curse;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "thump-service-curse", version = Reference.MOD_VERSION, useMetadata = true)
+public class CursePluginMod {
+}
